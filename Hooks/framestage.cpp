4f7eb5a785d1614215c1eb59169e27af7058063d
sonicrules11 <sonic886310@gmail.com>
2018-11-14 14:21:16 -0700

    Removed framestage

--- Hooks/framestage.cpp
+++
@@ -1,9 +0,0 @@
-#include "../main.h"
-
-bool bOnce = false; /*https://www.unknowncheats.me/forum/1190911-post11.html */
-void hkFrameStage(void* thisptr, ClientFrameStage_t curStage) {
-    C_BaseEntity* pLocal = (C_BaseEntity*)pEntList->GetClientEntity(pEngine->GetLocalPlayer());
-
-    clientVMT->GetOriginalMethod<tFrameStage>(37)(thisptr, curStage);
-    
-}
